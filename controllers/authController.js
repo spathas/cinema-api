b8a5a97f3ce2dc4fca288d4fbf0032eb7cf9f90a
CSpathas <spathasx@gmail.com>
2021-09-19 22:33:27 +0300

    Export token for authorization. Fix issue!

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,6 +32,7 @@ const createAndSendToken = (user, statusCode, res) => {
 
   res.status(statusCode).json({
     status: 'success',
+    token,
     expires,
     data: {
       user
@@ -89,6 +90,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
