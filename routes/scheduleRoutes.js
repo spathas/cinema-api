76799c1721ec177fc175ed28f6ce293590c1ea60
cspathas1 <spathasx@gmail.com>
2021-10-24 15:57:43 +0300

    close restrict to admin by schedule update.

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -29,7 +29,7 @@ router
   )
   .patch(
     authController.protect,
-    authController.restrictTo('admin'),
+    // authController.restrictTo('admin'),
     scheduleController.updateReqBodyForNestedObjs,
     scheduleController.updateSchedule
   );
